a51f642c465d3beb5cd80d101bcde6b25bb5d3a8
jfj36 <jfj36@bath.ac.uk>
2024-12-03 18:45:25 +0000

    Instruction 6: change __init__.py to import the funtion non_globular_cluster

--- cluster_maker/__init__.py
+++ cluster_maker/__init__.py
@@ -7,6 +7,7 @@
 ## Make functions available to the user 
 from .dataframe_builder import (
     define_dataframe_structure,
-    simulate_data)
+    simulate_data,
+    non_globular_cluster)
 # Make function export_to_csv available to the user
 from .data_exporter import export_to_csv, export_formatted
