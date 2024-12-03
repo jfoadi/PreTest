431ed076a68f0849a5c4e16337fc1100ba8d50b1
jfj36 <jfj36@bath.ac.uk>
2024-12-03 18:26:11 +0000

    Instruction 5: Modify function export_formatted.py by adding useful overall information about the data

--- cluster_maker/data_exporter.py
+++ cluster_maker/data_exporter.py
@@ -7,6 +7,7 @@
 ###
 
 ## Function to export to CSV
+import pandas as pd
 def export_to_csv(data, filename, delimiter=",", include_index=False):
     """
     Export the DataFrame to a CSV file.
@@ -27,6 +28,8 @@ def export_to_csv(data, filename, delimiter=",", include_index=False):
         print(f"Error exporting data to CSV: {e}")
 
 ## Function to export to formatted text file
+        
+        
 def export_formatted(data, filename):
     """
     Export the DataFrame to a formatted text file.
@@ -38,13 +41,39 @@ def export_formatted(data, filename):
     Returns:
         None
     """
+    def missing_values(data):
+        """
+        Calculate the percentage of missing values and zeros in each column of a given DataFrame.
+        
+        Parameters:
+        data (DataFrame): The input DataFrame.
+        
+        Returns:
+        DataFrame: A DataFrame containing the column name, number of unique values, percentage of missing values, and percentage of zeros.
+        """
+        df = pd.DataFrame()
+        for col in list(data):
+            unique_values = data[col].unique()
+            try:
+                unique_values = np.sort(unique_values)
+            except:
+                pass
+            nans = round(pd.isna(data[col]).sum()/data.shape[0]*100,1 )
+            zeros = round( (data[col]==0).sum()/data.shape[0]*100,1 )
+            df = pd.concat([df, pd.DataFrame([col, len(unique_values), nans, zeros]).T])
+        return df.rename(columns={0:'variable',1:'Unique values',2:'Nan %',3:'zeros %'}).sort_values('Nan %', ascending=False)
+
+    df_info = missing_values(data)
     try:
         with open("data/"+filename, 'w') as file:
-            file.write(str(data))
+            file.write(f"Data information:\n\n")
+            file.write(f"Number of rows: {data.shape[0]}\n")
+            file.write(f"Number of columns: {data.shape[1]}\n\n ")  
+
+            df_info.to_string(file)
+            file.write(f"\n\nData:\n\n")
+            data.to_string(file)
         print(f"Data successfully exported to {filename}")
     except Exception as e:
         print(f"Error exporting data to formatted text file: {e}")
-
-
-
 
