81147188b3378e198815abd84dfd1c9437571b9a
jfj36 <jfj36@bath.ac.uk>
2024-12-03 12:29:34 +0000

    Instruction 1: Add comment in the __init__.py file

--- cluster_maker/__init__.py
+++ cluster_maker/__init__.py
@@ -8,5 +8,5 @@
 from .dataframe_builder import (
     define_dataframe_structure,
     simulate_data)
-
+# Make function export_to_csv available to the user
 from .data_exporter import export_to_csv
